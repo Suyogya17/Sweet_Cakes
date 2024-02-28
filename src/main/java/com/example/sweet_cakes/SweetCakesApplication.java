f694958e8f9c4ae249619c6e0921531a780df22f
Suyogya17 <suyogyashrestha17@gmail.com>
2024-02-28 12:27:41 +0545

    first commit

---
+++ src/main/java/com/example/sweet_cakes/SweetCakesApplication.java
@@ -0,0 +1,13 @@
+package com.example.sweet_cakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetCakesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetCakesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sweet_cakes/SweetCakesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sweet_cakes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetCakesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
